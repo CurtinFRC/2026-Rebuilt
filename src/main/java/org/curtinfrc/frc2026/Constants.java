1abde75c2a82d3f625259975aaa3d4309cd404d6
Jade Turner <spacey-sooty@proton.me>
2025-12-22 01:56:53 +0800

    Initial Commit
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

---
+++ src/main/java/org/curtinfrc/frc2026/Constants.java
@@ -0,0 +1,33 @@
+package org.curtinfrc.frc2026;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Constants {
+  public static final RobotType robotType = RobotType.SIM;
+
+  public static final Mode getMode() {
+    return switch (robotType) {
+      case COMP, DEV -> RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
+      case SIM -> Mode.SIM;
+    };
+  }
+
+  public static enum Mode {
+    REAL,
+    SIM,
+    REPLAY
+  }
+
+  public static enum RobotType {
+    COMP,
+    DEV,
+    SIM
+  }
+
+  public static void main(String... args) {
+    if (robotType == RobotType.SIM) {
+      System.out.println("Error invalid robot type selected for deploy: SIM");
+      System.exit(1);
+    }
+  }
+}
