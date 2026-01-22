f665f4c9328ebf4b0b46b4c032b3af4a9ab01a8c
Eason-F <eason.ycfeng@outlook.com>
2026-01-22 15:13:06 +0800

    feature: implement hooded-shooter
    Co-authored-by: Sean Chan <seanyk.chan@gmail.com>

--- src/main/java/org/curtinfrc/frc2026/subsystems/hoodedshooter/ShooterIODev.java
+++ src/main/java/org/curtinfrc/frc2026/subsystems/hoodedshooter/ShooterIODev.java
@@ -2,6 +2,8 @@ package org.curtinfrc.frc2026.subsystems.hoodedshooter;
 
 import static org.curtinfrc.frc2026.util.PhoenixUtil.tryUntilOk;
 
+import org.curtinfrc.frc2026.util.PhoenixUtil;
+
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
@@ -16,66 +18,63 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
-import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
-import java.util.List;
-import org.curtinfrc.frc2026.util.PhoenixUtil;
 
 public class ShooterIODev implements ShooterIO {
-  public static final int ID1 = 18;
-  public static final int ID2 = 19;
-  public static final int ID3 = 29;
+  public static final int ID1 = 10;
+  public static final int ID2 = 11;
+  public static final int ID3 = 12;
+  public static final int ID4 = 13;
 
   public static final double GEAR_RATIO = 1.0;
-  private static final double KP = 0.00076245;
-  private static final double KI = 0.0;
-  private static final double KD = 0.1;
-  private static final double KS = 0.24152;
-  private static final double KV = 0.12173;
-  private static final double KA = 0.015427;
+  private static final double K_P = 6;
+  private static final double K_I = 0.0;
+  private static final double K_D = 0.1;
+  private static final double K_S = 0.1; // adds additional voltage to account for friction
+  private static final double K_V = 0.125;
 
   protected final TalonFX leaderMotor = new TalonFX(ID1);
   protected final TalonFX followerMotor1 = new TalonFX(ID2);
   protected final TalonFX followerMotor2 = new TalonFX(ID3);
+  protected final TalonFX followerMotor3 = new TalonFX(ID4);
 
   private final TalonFXConfiguration sharedMotorConfig =
       new TalonFXConfiguration()
-          .withMotorOutput(
-              new MotorOutputConfigs()
-                  .withNeutralMode(NeutralModeValue.Coast)
-                  .withInverted(InvertedValue.Clockwise_Positive))
+          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake))
           .withCurrentLimits(
-              new CurrentLimitsConfigs().withSupplyCurrentLimit(100).withStatorCurrentLimit(120))
+              new CurrentLimitsConfigs().withSupplyCurrentLimit(30).withStatorCurrentLimit(60))
           .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(GEAR_RATIO))
           .withSlot0(
-              new Slot0Configs().withKP(KP).withKI(KI).withKD(KD).withKS(KS).withKV(KV).withKA(KA));
+              new Slot0Configs().withKP(K_P).withKI(K_I).withKD(K_D).withKS(K_S).withKV(K_V));
+
+  private final TalonFXConfiguration leftMotorConfig =
+      sharedMotorConfig.withMotorOutput(
+          sharedMotorConfig.MotorOutput.withInverted(InvertedValue.Clockwise_Positive));
+
+  private final TalonFXConfiguration rightMotorConfig =
+      sharedMotorConfig.withMotorOutput(
+          sharedMotorConfig.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive));
 
   private final StatusSignal<Voltage> voltage = leaderMotor.getMotorVoltage();
   private final StatusSignal<Current> current = leaderMotor.getStatorCurrent();
   private final StatusSignal<AngularVelocity> velocity = leaderMotor.getVelocity();
   private final StatusSignal<AngularAcceleration> acceleration = leaderMotor.getAcceleration();
-  private final StatusSignal<Angle> position = leaderMotor.getPosition();
-
-  private final List<StatusSignal<Temperature>> motorTemperatures =
-      List.of(
-          leaderMotor.getDeviceTemp(),
-          followerMotor1.getDeviceTemp(),
-          followerMotor2.getDeviceTemp());
 
   final VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(true);
   final VelocityVoltage velocityRequest = new VelocityVoltage(0).withEnableFOC(true).withSlot(0);
 
   public ShooterIODev() {
-    tryUntilOk(5, () -> leaderMotor.getConfigurator().apply(sharedMotorConfig));
-    tryUntilOk(5, () -> followerMotor1.getConfigurator().apply(sharedMotorConfig));
-    tryUntilOk(5, () -> followerMotor2.getConfigurator().apply(sharedMotorConfig));
+    tryUntilOk(5, () -> leaderMotor.getConfigurator().apply(leftMotorConfig));
+    tryUntilOk(5, () -> followerMotor1.getConfigurator().apply(leftMotorConfig));
+    tryUntilOk(5, () -> followerMotor2.getConfigurator().apply(rightMotorConfig));
+    tryUntilOk(5, () -> followerMotor3.getConfigurator().apply(rightMotorConfig));
 
-    followerMotor1.setControl(new Follower(ID1, MotorAlignmentValue.Opposed));
+    followerMotor1.setControl(new Follower(ID1, MotorAlignmentValue.Aligned));
     followerMotor2.setControl(new Follower(ID1, MotorAlignmentValue.Opposed));
+    followerMotor3.setControl(new Follower(ID1, MotorAlignmentValue.Opposed));
 
     BaseStatusSignal.setUpdateFrequencyForAll(50.0, velocity, acceleration, voltage, current);
     leaderMotor.optimizeBusUtilization();
@@ -84,28 +83,15 @@ public class ShooterIODev implements ShooterIO {
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
-    inputs.motorTemperatures = new double[3];
-    inputs.motorsConnected = new boolean[3];
-    for (int motor = 0; motor < 3; motor++) {
-      inputs.motorTemperatures[motor] = motorTemperatures.get(motor).getValueAsDouble();
-      inputs.motorsConnected[motor] = motorTemperatures.get(motor).getStatus().isOK();
-    }
-
     inputs.appliedVolts = voltage.getValueAsDouble();
     inputs.currentAmps = current.getValueAsDouble();
-    inputs.positionRotations = position.getValueAsDouble();
-    inputs.velocityMetresPerSecond = convertRPSToVelocity(velocity.getValueAsDouble());
+    inputs.angularVelocityMetresPerSecond = convertRPSToVelocity(velocity.getValueAsDouble());
     inputs.accelerationMetresPerSecondPerSecond =
         convertRPSToVelocity(acceleration.getValueAsDouble());
   }
 
   @Override
   public void setVoltage(double voltage) {
-    leaderMotor.setControl(voltageRequest.withOutput(voltage));
-  }
-
-  @Override
-  public void setVoltageV(Voltage voltage) {
     leaderMotor.setControl(voltageRequest.withOutput(voltage));
   }
 
