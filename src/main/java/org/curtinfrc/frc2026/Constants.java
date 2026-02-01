8804fad331a3cf0a506fdb6c119904594b88e11e
Eason-F <eason.ycfeng@outlook.com>
2026-02-01 13:21:39 +0800

    chore: removed unnecessary features

--- src/main/java/org/curtinfrc/frc2026/Constants.java
+++ src/main/java/org/curtinfrc/frc2026/Constants.java
@@ -10,8 +10,6 @@ public final class Constants {
 
   public static final RobotType robotType = RobotType.DEV;
 
-  public static final boolean disabledHal = true; // not too sure
-
   public static final Mode getMode() {
     return switch (robotType) {
       case COMP, DEV -> RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
