a5b9c3468489ebb716824954b82a5ada1e4300aa
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2026-02-02 19:27:18 +0800

    a

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -111,6 +111,13 @@ public class Robot extends LoggedRobot {
   private final CommandXboxController controller = new CommandXboxController(0);
   private final Alert controllerDisconnected =
       new Alert("Driver controller disconnected!", AlertType.kError);
+  private final NetworkTablesValue<Double> shotAngle =
+      NetworkTablesValue.ofDouble(
+          NetworkTableInstance.getDefault(), NetworkTablesValue.toAdvantageKit("/ShotAngle"), 0.0);
+
+  private final NetworkTablesValue<Double> shotSpeed =
+      NetworkTablesValue.ofDouble(
+          NetworkTableInstance.getDefault(), NetworkTablesValue.toAdvantageKit("/ShotSpeed"), 0.0);
 
   @AutoLogOutput(key = "Triggers/IsRed")
   Trigger isRed = new Trigger(() -> DriverStation.getAlliance().get().equals(Alliance.Red));
@@ -297,8 +304,6 @@ public class Robot extends LoggedRobot {
                   new ModuleIOSim(TunerConstants.FrontRight),
                   new ModuleIOSim(TunerConstants.BackLeft),
                   new ModuleIOSim(TunerConstants.BackRight));
-
-          drive.setPose(new Pose2d(14, 4, new Rotation2d()));
 
           vision =
               new Vision(
