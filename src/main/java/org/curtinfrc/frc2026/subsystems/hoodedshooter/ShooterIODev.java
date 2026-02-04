d7059ec8c7ebcf2030ba27fa7492544ccd9e6c22
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2026-02-04 13:52:32 +0800

    camera configs

--- src/main/java/org/curtinfrc/frc2026/subsystems/hoodedshooter/ShooterIODev.java
+++ src/main/java/org/curtinfrc/frc2026/subsystems/hoodedshooter/ShooterIODev.java
@@ -42,8 +42,9 @@ public class ShooterIODev implements ShooterIO {
   private static final double KI = 0.001;
   private static final double KD = 0.004;
   private static final double KS = 0.24152;
-  private static final double KV = 0.126;
-  private static final double KA = 0.0;
+  private static final double KV = 0.12173;
+  private static final double KA = 0.015427;
+  private static final double EFFICIENCY = 0.8;
 
   protected final TalonFX leaderMotor = new TalonFX(ID1);
   protected final TalonFX followerMotor1 = new TalonFX(ID2);
@@ -130,7 +131,7 @@ public class ShooterIODev implements ShooterIO {
 
   @Override
   public void setVelocity(double velocity, BooleanSupplier f) {
-    double rps = convertVelocityToRPS(velocity);
+    double rps = convertVelocityToRPS(velocity / EFFICIENCY);
     int slot = f.getAsBoolean() ? 1 : 0;
     if (f.getAsBoolean()) {
       leaderMotor.setControl(
