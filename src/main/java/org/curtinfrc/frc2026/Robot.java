49d78863a230be0e3445a5f401cb965e0f4b5bd3
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2026-02-12 18:22:48 +0800

    fixed for now i guess

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -521,7 +521,7 @@ public class Robot extends LoggedRobot {
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
-    wireRepulsor();
+    // wireRepulsor();
   }
 
   /** This function is called periodically during operator control. */
