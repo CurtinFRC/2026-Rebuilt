d65f76f956ed95f3343ce0067b178c7868bd55e8
Jason <jy201016@gmail.com>
2026-02-28 20:55:07 +0800

    Added new tuning file, tested driving, and added triggers for autonomous hub facing

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -229,10 +229,11 @@ public class Robot extends LoggedRobot {
             drive.joystickDrive(
                 () -> -controller.getLeftY(),
                 () -> -controller.getLeftX(),
-                () -> -controller.getRightX()),
-            intake.RawControlConsume(1.0),
-            mag.store(0.7),
-            Commands.defer(() -> mag.holdIndexerCommand(), Set.of(mag))));
+                () -> -controller.getRightX())
+            // intake.RawControlConsume(1.0),
+            // mag.store(0.7),
+            // Commands.defer(() -> mag.holdIndexerCommand(), Set.of(mag))
+            ));
     Trigger automaticLocation =
         new Trigger(
             () -> {
