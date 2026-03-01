dc1ba4e7867cb80ae27a4b23bb67fe781ccc396f
prawny-boy <seanyk.chan@gmail.com>
2026-03-01 17:24:59 +0800

    feature: prepare for tuning sfa and sotm

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -5,6 +5,7 @@ import static org.curtinfrc.frc2026.vision.Vision.devCameraConfigs;
 
 import choreo.auto.AutoFactory;
 import com.ctre.phoenix6.SignalLogger;
+import org.curtinfrc.frc2026.util.LoggedNetworkStruct;
 import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.net.WebServer;
@@ -65,6 +66,7 @@ import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+import edu.wpi.first.math.geometry.Translation2d;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -84,6 +86,8 @@ public class Robot extends LoggedRobot {
   private final CommandXboxController controller = new CommandXboxController(0);
   private final Alert controllerDisconnected =
       new Alert("Driver controller disconnected!", AlertType.kError);
+  
+  private LoggedNetworkStruct<Translation2d> shootTargetPose = new LoggedNetworkStruct<Translation2d>("ShootTargetPose", HoodedShooter.HUB_LOCATION);
 
   private LoggedNetworkStruct<Translation2d> shootTargetPose =
       new LoggedNetworkStruct<Translation2d>("ShootTargetPose", HoodedShooter.HUB_LOCATION);
