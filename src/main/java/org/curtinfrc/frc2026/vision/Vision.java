bdec3053d7f875182faed24f1a70f6320a6950a6
Jason <jy201016@gmail.com>
2026-03-07 12:01:01 +0800

    Autonomous turning to location now works, changed it so that triggers start when the robot is enabled.

--- src/main/java/org/curtinfrc/frc2026/vision/Vision.java
+++ src/main/java/org/curtinfrc/frc2026/vision/Vision.java
@@ -74,14 +74,14 @@ public class Vision extends VirtualSubsystem {
         new CameraConfig(
             "Shooter Left",
             new Transform3d(
-                new Translation3d(0.21424577, -0.27179598, 0.36319361),
-                new Rotation3d(0, -0.388662903083, 0.174533)),
+                new Translation3d(0.21424577, .28474389, 0.36319361),
+                new Rotation3d(0, -0.388662903083, 0.174533 + Math.PI)),
             1.0),
         new CameraConfig(
             "Shooter Right",
             new Transform3d(
-                new Translation3d(0.21503940, 0.28474389, 0.36355173),
-                new Rotation3d(0, -0.388662903083, -0.174533)),
+                new Translation3d(0.21503940, -0.271795980, 0.36355173),
+                new Rotation3d(0, -0.388662903083, -0.174533 + Math.PI)),
             1.0),
       };
 
